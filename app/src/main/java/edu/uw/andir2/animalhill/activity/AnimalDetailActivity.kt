c659ede1daa93a5c58fd9de87eada86558165773
cocoli <kel28@uw.edu>
2021-06-08 11:19:56 +0800

    updated animal list

--- app/src/main/java/edu/uw/andir2/animalhill/activity/AnimalDetailActivity.kt
+++ app/src/main/java/edu/uw/andir2/animalhill/activity/AnimalDetailActivity.kt
@@ -1,4 +1,32 @@
 package edu.uw.andir2.animalhill.activity
 
-class AnimalDetailActivity {
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.uw.andir2.animalhill.databinding.ActivityAnimalDetailBinding
+
+const val NAME = "name"
+
+fun navigateToDetailActivity(context: Context, name: String) {
+    val intent = Intent(context, AnimalDetailActivity::class.java)
+    val bundle = Bundle().apply {
+        putString(NAME, name)
+    }
+    intent.putExtras(bundle)
+    context.startActivity(intent)
+}
+
+class AnimalDetailActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityAnimalDetailBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        with(binding) {
+            val currentName = intent.getStringExtra(NAME)
+            name.text = currentName
+        }
+    }
 }
--- app/src/main/java/edu/uw/andir2/animalhill/activity/AnimalListActivity.kt
+++ app/src/main/java/edu/uw/andir2/animalhill/activity/AnimalListActivity.kt
@@ -1,4 +1,21 @@
 package edu.uw.andir2.animalhill.activity
 
-class AnimalListActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.uw.andir2.animalhill.databinding.ActivityAnimalListBinding
+
+class AnimalListActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityAnimalListBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        with(binding) {
+            imageView.setOnClickListener {
+                navigateToDetailActivity(this@AnimalListActivity, "new animal")
+            }
+        }
+    }
+
 }
